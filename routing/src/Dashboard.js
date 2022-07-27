b7e45cb1c8c6e3431f3fc66112f9453d6ce0de17
Mansi <mansiakbari01@gmail.com>
2022-07-27 16:25:10 +0530

    Link with Rout

---
+++ routing/src/Dashboard.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+const Dashboard = () => {
+  return <div>Im Dashboard Page</div>;
+};
+export default Dashboard;
---
+++ routing/src/Login.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+const Login = () => {
+  return <div>i'm Login Page</div>;
+};
+export default Login;
